c61088ca2c42c524f6e17c10ca7c9e2534c1ecbe
liuxi <625353861@qq.com>
2019-12-18 18:28:45 +0800

    add gold mining

---
+++ xiaohui/src/main/java/com/fantasybaby/xiaohui/chapter5/GoldMining.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Copyright (C) 2018 ShangHai Quicktron Intelligent Technology Co.,Ltd
+ * All Rights Reserved.
+ * 本软件为上海快仓智能科技开发研制。未经本公司正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.fantasybaby.xiaohui.chapter5;
+
+/**
+ * 金矿问题
+ * 求出
+ * g 是每座金矿的矿数量
+ * p 为每个矿要多少人
+ * @author reid.liu
+ * @date 2019-12-18 17:22
+ */
+public class GoldMining {
+    public int biggestGold(int n,int w,int[]g,int[] p){
+        return 0;
+    }
+}
