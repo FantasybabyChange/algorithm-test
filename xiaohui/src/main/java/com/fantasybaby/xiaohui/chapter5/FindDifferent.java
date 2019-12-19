7f76ff649e340a015cc030d0d8f7f8d13f3a76c7
liuxi <625353861@qq.com>
2019-12-19 17:05:00 +0800

    add xor

---
+++ xiaohui/src/main/java/com/fantasybaby/xiaohui/chapter5/FindDifferent.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright (C) 2018 ShangHai Quicktron Intelligent Technology Co.,Ltd
+ * All Rights Reserved.
+ * 本软件为上海快仓智能科技开发研制。未经本公司正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.fantasybaby.xiaohui.chapter5;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组
+ * 其中 有两个数出现了奇数次 其他数都出现了偶数次
+ * 求这两个数
+ * @author reid.liu
+ * @date 2019-12-19 16:17
+ */
+public class FindDifferent {
+    public int[] find(int[] input){
+        int result = input[0];
+        for (int i = 1; i < input.length; i++) {
+            result =result^ input[i];
+        }
+        int index = 1;
+        while ((result & index) == 0){
+            index<<=1;
+
+        }
+        int[] an = new int[2];
+        for (int i = 0; i < input.length; i++) {
+            if((input[i]& index) == 0){
+                an[0] =an[0] ^ input[i];
+            }else{
+                an[1] =an[1] ^ input[i];
+            }
+
+        }
+        return an;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4,1,2,2,5,1,4,3};
+        int[] result = new FindDifferent().find(array);
+        System.out.println(Arrays.toString(result));
+    }
+}
