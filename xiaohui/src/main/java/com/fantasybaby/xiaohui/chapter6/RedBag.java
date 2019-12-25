c95ee3ff684c5fe89a36e2ba1c52633e3ec29254
fantasybaby <625353861@qq.com>
2019-12-25 21:28:06 +0800

    add segment

--- xiaohui/src/main/java/com/fantasybaby/xiaohui/chapter6/RedBag.java
+++ xiaohui/src/main/java/com/fantasybaby/xiaohui/chapter6/RedBag.java
@@ -1,7 +1,12 @@
 package com.fantasybaby.xiaohui.chapter6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * @author: liuxi
@@ -21,6 +26,34 @@ public class RedBag {
             restM -=i2;
         }
         return result;
+    }
+    public List<Integer> dividedRedBag1(int totalAmount,int totalPeopleNum){
+        List<Integer> amountList = new ArrayList();
+        Set<Integer> segments = new HashSet();
+        Random random = new Random();
+        for(int i = 0; i< totalPeopleNum-1; i++){
+            int segment =  random.nextInt(totalAmount-2) + 1;
+            int delta = random.nextInt(1)==0 ? 1 : -1;
+            while(segments.contains(segment) || segment == 0){
+                segment = (segment+delta)%totalAmount;
+            }
+            segments.add(segment);
+        }
+
+        List<Integer> segmentList = new ArrayList(segments);
+        Collections.sort(segmentList);
+        for(int i=0; i<segmentList.size(); i++){
+            Integer amount;
+            if(i==0){
+                amount = segmentList.get(0);
+            }else {
+                amount = segmentList.get(i) - segmentList.get(i-1) ;
+            }
+            amountList.add(amount);
+        }
+        amountList.add(totalAmount - segmentList.get(segmentList.size()-1));
+
+        return amountList;
     }
 
     public static void main(String[] args) {
