e97e4469531cc4339bbfe4cc060453a20aa9f884
fantasybaby <625353861@qq.com>
2020-01-09 10:00:09 +0800

    merge test code

--- leetcode-test/src/main/java/com/fantasybaby/leetcode/sort/ZigzagConversion.java
+++ leetcode-test/src/main/java/com/fantasybaby/leetcode/sort/ZigzagConversion.java
@@ -100,11 +100,17 @@ public class ZigzagConversion {
         if (numRows == 1) return s;
         StringBuilder ret = new StringBuilder();
         int n = s.length();
+        /**
+         * 计算出步数
+         */
         int cycleLen = 2 * numRows - 2;
 
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++) { //行数
             for (int j = 0; j + i < n; j += cycleLen) {
                 ret.append(s.charAt(j + i));
+                /**
+                 *
+                 */
                 if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
                     ret.append(s.charAt(j + cycleLen - i));
             }
