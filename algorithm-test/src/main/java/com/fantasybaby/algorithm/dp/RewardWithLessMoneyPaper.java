e18195903a5a464b222b505c11ea754076bc4641
Reid Liu <Reid.liu@kuka.com>
2021-03-04 17:54:22 +0800

    remove all comment

--- algorithm-test/src/main/java/com/fantasybaby/algorithm/dp/RewardWithLessMoneyPaper.java
+++ algorithm-test/src/main/java/com/fantasybaby/algorithm/dp/RewardWithLessMoneyPaper.java
@@ -1,14 +1,9 @@
-/******************************************************************************
- * Copyright (C) 2018 ShangHai Quicktron Intelligent Technology Co.,Ltd
- * All Rights Reserved.
- * 本软件为上海快仓智能科技开发研制。未经本公司正式书面同意，其他任何个人、团体
- * 不得使用、复制、修改或发布本软件.
- *****************************************************************************/
+
 package com.fantasybaby.algorithm.dp;
 
-import com.google.common.collect.Lists;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**给定总金额
  * 和货币总数
